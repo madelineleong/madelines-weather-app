79955076309bf060cf1c0eb9a0f8a84aab594349
madelineleong <116978350+madelineleong@users.noreply.github.com>
2022-11-07 16:43:57 -0500

    added icon from API

--- src/app.js
+++ src/app.js
@@ -28,15 +28,23 @@ function displayTemperature(response) {
   let currentHumidity = document.querySelector("#humidity");
   let currentWind = document.querySelector("#wind");
   let dateElement = document.querySelector("#current-date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   currentDescription.innerHTML = response.data.weather[0].description;
   currentHumidity.innerHTML = response.data.main.humidity;
   currentWind.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Akron&appid=${apiKey}&units=metric`;
+let city = "Portland";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
