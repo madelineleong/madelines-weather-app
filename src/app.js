fb1a6cc0abe1a8da7bab5e84149492be41ed535d
madelineleong <116978350+madelineleong@users.noreply.github.com>
2022-11-07 16:44:58 -0500

    updated city

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-let city = "Portland";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
