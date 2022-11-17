8f673514dd9c01ba54437019ffe21301089fc601
madelineleong <116978350+madelineleong@users.noreply.github.com>
2022-11-16 21:24:37 -0500

    updated forecast java dates

--- src/app.js
+++ src/app.js
@@ -21,6 +21,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -28,22 +36,30 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="forecast-date">${forecastDay.dt}</div>
+      <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
-         src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+         src="http://openweathermap.org/img/wn/${
+           forecastDay.weather[0].icon
+         }@2x.png"
         alt="thursday"
         width="40"
       />
       <div class="weather-forecast-temperature">
-        <span class="max-temperature-forecast">${forecastDay.temp.max}°</span>
-        <span class="min-temperature-forecast">${forecastDay.temp.min}°</span>
+        <span class="max-temperature-forecast">${Math.round(
+          forecastDay.temp.max
+        )}°</span>
+        <span class="min-temperature-forecast">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
       </div>
     </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
